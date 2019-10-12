1987880e35488ccd99c72073d46c8e6820d5b5b5
Matt Joseph <mattomjoe@yahoo.com>
2019-10-12 10:29:47 -0500

    Finalize directory layout

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('El Ranchero', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whiskey River BBQ', true);
