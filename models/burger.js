32936853ce717f60abba3be6c5c91cd236b8c16a
Matt Joseph <mattomjoe@yahoo.com>
2019-10-16 22:08:31 -0500

    Revise code based on Cats app, successfully adds new burger to mySQL database table.

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,33 @@
-Inside burger.js, import orm.js into burger.js
+// Inside burger.js, import orm.js into burger.js - DONE
 
+// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM. - DONE
 
-Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+// Export at the end of the burger.js file. - DONE
 
+var orm = require("../config/orm.js");
 
-Export at the end of the burger.js file.
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    deleteOne: function(condition, cb) {
+        orm.deleteOne("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
 
--- server.js
+++ server.js
@@ -1,41 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+
+var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended: true
 }));
-app.use(methodOverride("_method"));
+
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
-var port = 3000;
-app.listen(port);
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var routes = require("./controllers/burgers_controller.js");
 
-connection.connect(function(err) {
-    if(err)throw err;
-    console.log("Connected as id: " + connection.threadId);
-});
+app.use(routes);
 
-// Create Express route to post mySQL database data to web page
-app.get("/", function(req,res){
-    connection.query("SELECT * FROM burgers;", function(err,data){
-        res.render("index", {burgers:data});
-    });
-});
-
-app.post("/create", function(req,res){
-    connection.query("INSERT INTO burgers (burger_name) VALUES (?);", [req.body.burger_name], function(err,result){
-        if(err)throw err;
-        res.redirect("/");
-    });
+app.listen(PORT, function() {
+    console.log("Server listening on http://localhost: " + PORT);
 });
